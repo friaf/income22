1993a72e41aaea49034a1dc0a19f42f0916eaef4
friaf <shovel.irena@gmail.com>
2022-11-13 01:54:28 +0000

    Wire up APIs and confirm access to google sheet

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("income_22")
+
+income_august_22 = SHEET.worksheet("income_august_22")
+income_september_22 = SHEET.worksheet("income_september_22")
+income_october_22 = SHEET.worksheet("income_october_22")
+
+august_data = income_august_22.get_all_values()
+september_data = income_september_22.get_all_values()
+october_data = income_october_22.get_all_values()
+
+print(august_data)
+
